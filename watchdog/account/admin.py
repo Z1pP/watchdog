7af7762c6e47be234d9fcab9957fbe198ed30f35
Z1pP <maxm_99@mail.ru>
2026-02-16 01:53:05 +0300

    Add admin interfaces for User and AuthProvider models in account app

--- watchdog/account/admin.py
+++ watchdog/account/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import AuthProvider, User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ["email", "is_staff", "is_active"]
+    list_filter = ["is_staff", "is_active"]
+    search_fields = ["email", "username"]
+    ordering = ["email"]
+
+
+@admin.register(AuthProvider)
+class AuthProviderAdmin(admin.ModelAdmin):
+    list_display = ["user", "provider", "provider_user_id", "created_at"]
+    list_filter = ["provider"]
+    search_fields = ["provider_user_id", "user__email"]
+    raw_id_fields = ["user"]
