60cdc5e08e89b5789ce1de657c89036c9481deb7
Z1pP <maxm_99@mail.ru>
2026-02-16 01:46:21 +0300

    Configure settings to include account app and set custom user model

--- watchdog/watchdog/settings.py
+++ watchdog/watchdog/settings.py
@@ -43,7 +43,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "account",
 ]
+
+AUTH_USER_MODEL = "account.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
