68c84a49e086a8efc394cf4ab193dbb4d4b172b2
Z1pP <maxm_99@mail.ru>
2026-02-16 03:59:02 +0300

    Add verbose names for AccountConfig in account app

--- watchdog/account/apps.py
+++ watchdog/account/apps.py
@@ -2,4 +2,7 @@ from django.apps import AppConfig
 
 
 class AccountConfig(AppConfig):
-    name = 'account'
+    name = "account"
+
+    verbose_name = "Аккаунты"
+    verbose_name_plural = "Аккаунты"
